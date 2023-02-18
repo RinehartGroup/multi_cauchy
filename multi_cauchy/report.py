0649bde15871b5e5aa254993415704a2dbf47d82
Philip Bunting <77863580+pcb7445@users.noreply.github.com>
2023-02-17 17:38:38 -0800

    report.py setup

---
+++ multi_cauchy/report.py
@@ -0,0 +1,8 @@
+"""
+In addition to recording the analysis results in a standardarized, easily retrievable
+format, the report should contain sufficient information regarding the data processing
+and fit input parameters to reproduce the results.
+
+There may also need to be some light data verification. One item in particular is a
+check that the sweep rate is constant.
+"""
